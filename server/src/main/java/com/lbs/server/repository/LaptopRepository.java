0778857a1ef0dd244f552ddfcf938feb2c85f897
Mortymier <sanielmg@gmail.com>
2025-10-18 18:22:46 +0800

    Server: add function to search for laptop using brand and model in LaptopRepository

--- server/src/main/java/com/lbs/server/repository/LaptopRepository.java
+++ server/src/main/java/com/lbs/server/repository/LaptopRepository.java
@@ -4,6 +4,7 @@ import com.lbs.server.entity.LaptopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LaptopRepository extends JpaRepository<LaptopEntity, Long>
@@ -13,4 +14,7 @@ public interface LaptopRepository extends JpaRepository<LaptopEntity, Long>
 
     // Find all laptops based on a 'brand' filter
     List<LaptopEntity> findByBrand(String brand);
+
+    // Find a specific laptop based on the brand and model
+    Optional<LaptopEntity> findByBrandAndModel(String brand, String model);
 }
