de9e41ca0caeff4b1273023643b379f8073e3c7f
Mortymier <sanielmg@gmail.com>
2025-10-18 18:11:16 +0800

    Server: fix mistake in function of StaffRepository

--- server/src/main/java/com/lbs/server/repository/StaffRepository.java
+++ server/src/main/java/com/lbs/server/repository/StaffRepository.java
@@ -1,7 +1,6 @@
 package com.lbs.server.repository;
 
 import com.lbs.server.entity.StaffEntity;
-import com.lbs.server.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -9,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface StaffRepository extends JpaRepository<StaffEntity, Long>
 {
-    Optional<StudentEntity> findByEmailAndPassword(String email, String password);
+    Optional<StaffEntity> findByEmailAndPassword(String email, String password);
 }
