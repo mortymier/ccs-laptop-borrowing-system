bcf433ca64bf65c2ddc3a5d2af7f570435f3c727
Mortymier <sanielmg@gmail.com>
2025-10-19 14:51:05 +0800

    Server: add initial Config layer

---
+++ server/src/main/java/com/lbs/server/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.lbs.server.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig
+{
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/students/**").permitAll()
+                .requestMatchers("/api/staffs/**").permitAll()
+                .requestMatchers("/api/laptops/**").permitAll()
+                .requestMatchers("/api/schedules/**").permitAll()
+                .requestMatchers("/api/borrows/**").permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
