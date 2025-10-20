6f406263446aef2123f98f96e6ed202abf09a673
Mortymier <sanielmg@gmail.com>
2025-10-21 00:02:49 +0800

    Client: add routes for About and Contact page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/Global.css';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
 
 export default function App()
 {
@@ -9,6 +11,8 @@ export default function App()
       <BrowserRouter>
         <Routes>
           <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
     </>
