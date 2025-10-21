acd652b282de33f9d7fcbd0495502711a6dd6dd0
Mortymier <sanielmg@gmail.com>
2025-10-21 10:53:21 +0800

    Client: add routes for Login and role registration pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import './styles/Global.css';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import RegisterStudent from './pages/RegisterStudent.jsx';
+import RegisterStaff from './pages/RegisterStaff.jsx';
 
 export default function App()
 {
@@ -13,6 +17,10 @@ export default function App()
           <Route index element={<Home/>}/>
           <Route path="about" element={<About/>}/>
           <Route path="contact" element={<Contact/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+          <Route path="registerstudent" element={<RegisterStudent/>}/>
+          <Route path="registerstaff" element={<RegisterStaff/>}/>
         </Routes>
       </BrowserRouter>
     </>
