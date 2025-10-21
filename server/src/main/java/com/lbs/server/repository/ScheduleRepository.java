5be420de196e8f4684bba1c30e4f20fc789bcbfa
Mortymier <sanielmg@gmail.com>
2025-10-21 20:13:37 +0800

    Server: add function for finding schedule using course and student in ScheduleRepository

--- server/src/main/java/com/lbs/server/repository/ScheduleRepository.java
+++ server/src/main/java/com/lbs/server/repository/ScheduleRepository.java
@@ -5,10 +5,14 @@ import com.lbs.server.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long>
 {
     // Find all schedules belonging to a specific student
     List<ScheduleEntity> findByStudent(StudentEntity student);
+    
+    // Find by course and student
+    Optional<ScheduleEntity> findByCourseAndStudent(String course, StudentEntity student);
 }
