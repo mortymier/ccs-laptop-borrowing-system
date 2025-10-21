514bf06bf6d184b0b18e8257a07127eb4d6cc29f
Mortymier <sanielmg@gmail.com>
2025-10-21 20:29:05 +0800

    Client: add initial BrowseLaptops page with styling and backend connection

---
+++ client/src/pages/BrowseLaptops.jsx
@@ -0,0 +1,201 @@
+import '../styles/BrowseLaptops.css';
+import StudentHeader from "../components/StudentHeader.jsx";
+import Footer from '../components/Footer.jsx';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function BrowseLaptops()
+{
+    const navigate = useNavigate();
+    const [laptops, setLaptops] = useState([]);
+    const [schedules, setSchedules] = useState([]);
+    const [showForm, setShowForm] = useState(false);
+    const [formLaptop, setFormLaptop] = useState({});
+
+    const [formInput, setFormInput] = useState
+    ({
+        reason: '',
+        scheduleCourse: ''
+    }); 
+
+    const showLaptops = laptops.map
+    (
+        (laptop, index) =>
+            <div key={index} className="laptop-card" onClick={() => handleClickCard(laptop)}>
+                <h2> {laptop.brand} - {laptop.model} </h2>
+                <p> RAM: {laptop.ram} </p>
+                <p> CPU: {laptop.cpu} </p>
+                <p> Storage: {laptop.storage} </p>
+                <p> Status: <span className={laptop.laptopstatus === 'AVAILABLE' ? 'status1' : 'status2'}>{laptop.laptopstatus}</span> </p>
+            </div>
+    );
+
+    const fetchAllLaptops = async() =>
+    {
+        try
+        {
+            const response = await fetch
+            (
+                'http://localhost:8080/api/laptops'
+            );
+
+            if(response.ok)
+            {
+                const fetchedLaptops = await response.json();
+                //console.log('Fetched laptops:', fetchedLaptops);
+                setLaptops(fetchedLaptops);
+            }
+            else
+            {
+                throw new Error('Failed to fetch laptops');
+            }
+        }
+        catch(error)
+        {
+            console.error(error);
+            alert(error.message);
+        }
+    };
+
+    const fetchStudentSchedules = async() =>
+    {
+        const loggedInStudent = localStorage.getItem('loggedInStudent');
+        const studentData = JSON.parse(loggedInStudent);
+        const studentEmail = studentData.email;
+
+        try
+        {
+            const response = await fetch
+            (
+                `http://localhost:8080/api/schedules/student?email=${encodeURIComponent(studentEmail)}`
+            );
+
+            if(response.ok)
+            {
+                const fetchedSchedules = await response.json();
+                setSchedules(fetchedSchedules);
+            }
+            else
+            {
+                throw new Error('Failed to fetch student schedules');
+            }
+        }
+        catch(error)
+        {
+            console.error(error);
+            alert(error.message);
+        }
+    };
+
+    const showSchedulesAsOptions = schedules.map
+    (
+        (schedule, index) =>
+            <option key={index} value={schedule.course}> {schedule.course} - {schedule.section} </option>
+    );
+
+    const handleClickCard = (laptop) =>
+    {
+        if(laptop.laptopstatus === 'AVAILABLE')
+        {
+            console.log(laptop);
+            setFormLaptop(laptop);
+            setShowForm(true);
+        }
+        else
+        {
+            alert('This laptop is not available for borrowing');
+        }
+    };
+
+    const handleCloseForm = () =>
+    {
+        setShowForm(false);
+        setFormLaptop({});
+    }
+
+    const handleSubmit = async(e) =>
+    {
+        e.preventDefault();
+
+        const loggedInStudent = localStorage.getItem('loggedInStudent');
+        const studentData = JSON.parse(loggedInStudent);
+        const studentEmail = studentData.email;
+        const scheduleCourse = formInput.scheduleCourse;
+        const laptopBrand = formLaptop.brand;
+        const laptopModel = formLaptop.model;
+
+        try
+        {
+            console.log(formInput);
+            console.log(formLaptop);
+
+            const response = await fetch
+            (
+                `http://localhost:8080/api/borrows/add?email=${encodeURIComponent(studentEmail)}&course=${encodeURIComponent(scheduleCourse)}&brand=${encodeURIComponent(laptopBrand)}&model=${encodeURIComponent(laptopModel)}`,
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(formInput)
+                }
+            );
+
+            if(response.ok) 
+            {
+                setFormLaptop({});
+                setFormInput({reason: '', scheduleCourse: ''})
+                setShowForm(false);
+                alert('Borrow request has been created!');
+            }
+            else
+            {
+                throw new Error('Request failed');
+            }
+        }
+        catch(error)
+        {
+            console.error(error);
+            alert(error.message);
+        }
+    }
+
+    useEffect(() => 
+    {
+        fetchAllLaptops();
+        fetchStudentSchedules();
+
+    }, [navigate, showForm, formInput]);
+
+    return (
+        <>
+            <title> Browse Laptops </title>
+            <StudentHeader/>
+            <main className="browse-laptops">
+                <div className="laptop-grid">
+                    {showLaptops}
+                </div>
+            </main>
+            { showForm ? 
+                <form className="borrowform" onSubmit={handleSubmit}>
+                    <h2> Borrow this laptop? </h2>
+                    <p>
+                        {formLaptop.brand} - {formLaptop.model} <br/>
+                        {formLaptop.ram}, {formLaptop.cpu}, {formLaptop.storage}
+                    </p>
+                    <label htmlFor="schedule"> Schedule </label>
+                    <select id="schedule" name="schedule" value={formInput.scheduleCourse} onChange={(e) => setFormInput((prev) => ({...prev, scheduleCourse: e.target.value}))} required>
+                        <option> </option>
+                        {showSchedulesAsOptions}
+                    </select>
+                    <br/>
+                    <label htmlFor="reason"> Reason </label>
+                    <textarea id="reason" name="reason" rows="5" value={formInput.reason} onChange={(e) => setFormInput((prev) => ({...prev, reason: e.target.value}))} required/>
+                    <div>
+                        <button id="submit" type="submit"> Submit </button>
+                        <button id="close" type="button" onClick={handleCloseForm}> Close </button>
+                    </div>
+                </form> : ''
+            }
+            <Footer/>
+        </>
+    );
+}
